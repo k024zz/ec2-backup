9cdc2b43ebdbc67b47d02b2cfee2e2870ec9ed33
loveSherlock <fanxz@me.com>
2016-04-11 13:14:58 -0400

    modified backup functions

--- ec2-backup.sh
+++ ec2-backup.sh
@@ -22,10 +22,118 @@ clean() {
 }
 
 # init 
-METHOD="dd"
-VOLFLAG="0"
-SSHFLAG="1"
-KEYNAME="ec2-backup-key"
+export METHOD="dd"
+export VOLFLAG="0"
+export SSHFLAG="1"
+export KEYNAME="ec2-backup-key"
+export INSTANCE_ADDRESS
+export origin_dir
+export EC2_BACKUP_FLAGS_SSH="-i ec2-backup.pem"
+export DIR="/MyBackUp"
+export backupDir="/ec2-back-up"
+
+make_file()
+{
+    count=0
+    while :
+    do
+        [ -n "${EC2_BACKUP_VERBOSE}"  ] && echo "make file..." >&2
+        sleep 3
+        ssh -q -t -oStrictHostKeyChecking=no $EC2_BACKUP_FLAGS_SSH ubuntu@ {INSTANCE_ADDRESS} "sudo mkfs.ext4 /dev/xvdf;"
+        if [ $? -eq 0  ];then
+            break
+        fi
+            count=$(($count+1))
+        if [ $cnt -eq 30  ];then
+            return 2
+        fi
+    done
+    echo "make file completed"
+}
+mount_DIR()
+{
+    if [ $VOLFLAG -eq 0  ];then
+    count=0
+    while :
+    do
+        [ -n "${EC2_BACKUP_VERBOSE}"  ] && echo "mount..." >&2
+        sleep 3
+        ssh -q -t -oStrictHostKeyChecking=no $EC2_BACKUP_FLAGS_SSH ubuntu@${INSTANCE_ADDRESS} "sudo mkdir $DIR"
+        ssh -q -t -oStrictHostKeyChecking=no $EC2_BACKUP_FLAGS_SSH ubuntu@${INSTANCE_ADDRESS} "sudo mount /dev/xvdf $DIR"
+        if [ $? -eq 0  ];then
+            break
+        fi
+        count=$(($count+1))
+        if [ $count -eq 30  ];then
+            return 2
+        fi
+    done
+    fi
+    echo "mount completed"
+
+}
+umount_DIR()
+{
+    count=0
+    while :
+    do
+        [ -n "${EC2_BACKUP_VERBOSE}"  ] && echo "umount..." >&2
+        sleep 3
+        ssh -q -t -oStrictHostKeyChecking=no $EC2_BACKUP_FLAGS_SSH ubuntu@${INSTANCE_ADDRESS} "sudo umount $DIR"
+        if [ $? -eq 0  ];then
+            break
+        fi
+        count=$(($count+1))
+        if [ $cnt -eq 30  ];then
+            return 2
+        fi
+    done
+    echo "umount completed"
+}
+back_up_dd()
+{
+    count=0
+    time=$(date | sed "s/ /-/g")
+    while :
+    do
+        [ -n "${EC2_BACKUP_VERBOSE}"  ] && echo "backing up! (dd)..." >&2
+        sleep 3
+        ssh -t -q -oStrictHostKeyChecking=no $EC2_BACKUP_FLAGS_SSH ubuntu@${INSTANCE_ADDRESS} "    sudo mkdir $DIR/$time/"
+        temp_name=$(echo $origin_dir |  sed "s/\//_/g")
+        tar -cPf - "$origin_dir" | ssh -q -t -oStrictHostKeyChecking=no $EC2_BACKUP_FLAGS_SSH ubuntu@${INSTANCE_ADDRESS} sudo dd of=$DIR/$time/$temp_name.tar.gz >/dev/null 2>&1
+        if [ $? -eq 0  ];then
+            break
+        fi
+        count=$(($count+1))
+        if [ $count -eq 30  ];then
+            return 2
+        fi
+    done
+    echo "back-dd"
+}
+
+back_up_rsync()
+{
+    count=0
+    time=$(date | sed "s/ /-/g")
+    while :
+        do
+        [ -n "${EC2_BACKUP_VERBOSE}"  ] && echo "mkfs and mount on $addr..." >&2
+        sleep 3
+        rsync -azP "$origin_dir" ubuntu@${INSTANCE_ADDRESS}:/home/ubuntu$backupDir -e "ssh ${EC2_BACKUP_FLAGS_SSH}" >/dev/null 2>&1
+        if [ $? -eq 0  ];then
+            break
+        fi
+    count=$(($count+1))
+    if [ $count -eq 30  ];then
+            return 2
+    fi
+    done
+    ssh -t -q -oStrictHostKeyChecking=no $EC2_BACKUP_FLAGS_SSH ubuntu@${INSTANCE_ADDRESS} "sudo mkdir $DIR/$time/"
+    ssh -t -q -oStrictHostKeyChecking=no $EC2_BACKUP_FLAGS_SSH ubuntu@${INSTANCE_ADDRESS} "sudo mv /home/ubuntu$backupDir/* $DIR/$time/"
+    echo "back-rsync"
+}
+
 
 # extract command line options with getopt
 while getopts :hm:v: opt
@@ -37,11 +145,11 @@ do
 	       echo "error: invalid method"
 		   exit 127
 	   fi
-	   echo "option m with value $METHOD" ;; 
+	   echo "option m with value $METHOD" ;;
 	v) VOLFLAG="1"
 	   VOLID=$OPTARG
 	   echo "option v with value $VOLID" ;;
-	*) echo "Unknown option: $opt" ;;	
+	*) echo "Unknown option: $opt" ;;
 	esac
 done
 shift `expr $OPTIND - 1`
@@ -53,7 +161,7 @@ if [ ! -d "$origin_dir" ]; then
 	echo "error: directory $origin_dir does not exist."
 	exit 1
 fi
-	
+
 
 # create a new security group for ec2 backup
 # check if the group ec2-backup exists
@@ -149,6 +257,11 @@ aws ec2 attach-volume --volume-id $VOLID --instance-id $instanceId --device /dev
 sleep 5 
 echo "attached"
 
+make_file
+mount_DIR
+back_up_dd
+back_up_rsync
+umount_DIR
 
 exit 0
 
